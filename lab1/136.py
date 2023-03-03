408c0400b90ddadc966500b8153e99d961b89777
Elphyn <vladkarelov14@gmail.com>
2023-03-03 16:30:07 +0900

    3/3/2023

--- lab1/136.py
+++ lab1/136.py
@@ -1,26 +1,41 @@
 # https://ivtipm.github.io/Programming/Glava06/index06.htm#z136
-# Даны натуральное число n, действительные числа a1,..., an. Вычислить: 
-import numpy as np
+# Даны натуральное число n, действительные числа a1,..., an. Вычислить:
+#  a1 - a2 + a3 - ... + (-1)^n+1an;
+# import numpy as np
+import random
 __author__ = "Vlad Karelov"
 
 
 # решение
-def main(n: int, a:list):
+def sum_arr(a):
+    """"""
     result = 0
-    for i in range(1, n):
+    for i in range(1, len(a)):
         result += (-1**i)*a[i]
     return result
         
 
-# ввод данных
-n = int(input("Enter n: "))
-# генерация массива
-data = np.random.uniform(1,10,n)
-print(data)
+def generate_list(n):
+    """Generates a list with random numbers n lenght"""
+    list = []
+    for i in range(n):
+        list.append(random.uniform(1,10))
+    return list
 
+def main():
+    # ввод данных
+    n = int(input("Enter n: "))
 
-print(f"Result: {main(n,data)}")
+    data = generate_list(n)
+
+    print(f"List we work with: {data}")
+    print(f"Result: {sum_arr(data)}")
+
+if __name__ == '__main__':
+    main()
 
 # функция заполнения масива рандомно✔️
 # добавить коментарии✔️
+
+# функция генерация списка, не использовать numpy✔️
 
--- lab1/181.py
+++ lab1/181.py
@@ -1,37 +1,39 @@
 # https://ivtipm.github.io/Programming/Glava01/index01.htm#z10
 # Даны целые числа a1,...a50. Получить сумму тех чисел данной последовательности, которые кратные 5
 __author__ = "Vlad Karelov"
-import numpy as np
-
-def sum_aliquot_numbers(array):
+# import numpy as np
+import random
+def sum_aliquot_numbers(list):
     """Takes array as an argument and returns sum of all numbers that are aliquot to 5"""
-    print(f"Array we work with: {array}")
+
+    # print(f"Array we work with: {array}")
     result = 0
 
-    for i in range(len(array)):
-        if array[i] % 5 == 0:
-            result += array[i]
+    for i in range(len(list)):
+        if list[i] % 5 == 0:
+            result += list[i]
     return result
 
 
 def create_array(n):
     """creates array of n length and fills it with random numbers from 1-100"""
-    data = np.zeros(n, dtype=int)
+    data = []
 
     for i in range(n):
-        dummy = np.random.randint(1,100)
-        data[i] = dummy
-
+        data.append(random.randint(1,10))
+        # dummy = np.random.randint(1,100)
+        # data[i] = dummy
     return data
 
 def main():
     """script execution"""
 
-    # create an array
-    array = create_array(50)
+    # create list
+    list = create_array(5)
+    print(f'Generated list: {list}')
 
     # sum all numbers that are aliquot to 5
-    result = sum_aliquot_numbers(array)
+    result = sum_aliquot_numbers(list)
 
     #print the result
     print(result)
@@ -39,10 +41,10 @@ def main():
 if __name__ == '__main__':
     main()
 
-
 # переделать проверка a кратности 5✔️
 # разбить на функции✔️
 
+# Сделать без numpy✔️
 
 
 
