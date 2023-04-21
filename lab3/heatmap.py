3e32ad401475bbcc5764e1be933580d1c7b86bbe
Elphyn <vladkarelov14@gmail.com>
2023-04-21 19:09:10 +0900

    somechanges

---
+++ lab3/heatmap.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate a random numpy matrix
+A = np.random.rand(50, 50)
+# Generate a random solution vector
+b = np.random.rand(50)
+
+# solve linalg
+x = np.linalg.solve(A, b)
+print(f'Result: {x}')
+
+# Heatmap matrix A
+plt.figure()
+sns.heatmap(A, cmap='coolwarm')
+plt.title('Heatmap of Matrix A')
+plt.savefig('heatmap_matrix_A.png')
+plt.show()
---
+++ lab3/histogram.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate a random numpy matrix
+A = np.random.rand(50, 50)
+# Generate a random solution vector
+b = np.random.rand(50)
+
+# solve linalg
+x = np.linalg.solve(A, b)
+print(f'Result: {x}')
+
+# Plot a histogram of the solution
+plt.figure()
+sns.histplot(x, kde=False)
+plt.title('Histogram of Solution')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.savefig('histogram_solution.png')
+plt.show()
